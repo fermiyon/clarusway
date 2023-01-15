1a94d924853291138ff00dab76fc1e2042ef6fe5
fermiyon <selmank@gmail.com>
2023-01-15 01:21:12 +0100

    sql-project

--- sql/project-1-mentoring/RDB&SQL_Student-1.sql
+++ sql/project-1-mentoring/RDB&SQL_Student-1.sql
@@ -63,22 +63,91 @@ GROUP BY
     YEAR(order_date)
 
 ---- 7. What is the sales quantity of product according to the brands and sort them highest-lowest----
+SELECT brand_name, SUM(quantity) sum
+FROM sale.order_item AS O
+JOIN product.product AS P
+ON O.product_id = P.product_id
+JOIN product.brand AS B
+ON P.brand_id = B.brand_id
+GROUP BY B.brand_name
+ORDER BY [sum] DESC
 
+SELECT brand_name, P.product_name, SUM(quantity) sum
+FROM sale.order_item AS O
+JOIN product.product AS P
+ON O.product_id = P.product_id
+JOIN product.brand AS B
+ON P.brand_id = B.brand_id
+GROUP BY B.brand_name, P.product_name
+ORDER BY [sum] DESC
 
 ---- 8. What are the categories that each brand has?----
+SELECT B.brand_name, C.category_name
+FROM product.product AS P
+JOIN product.category AS C
+ON P.category_id = C.category_id
+JOIN product.brand AS B
+ON P.brand_id = B.brand_id
+GROUP BY B.brand_name, C.category_name
 
 
 ---- 9. Select the avg prices according to brands and categories----
+SELECT B.brand_name, C.category_name, CONVERT(DECIMAL(18,2),(AVG(list_price)))
+FROM product.product AS P
+JOIN product.category AS C
+ON P.category_id = C.category_id
+JOIN product.brand AS B
+ON P.brand_id = B.brand_id
+GROUP BY B.brand_name, C.category_name
+ORDER BY B.brand_name, C.category_name
 
-
----- 10. Select the annual amount of product produced according to brands----
+----?? 10. Select the annual amount of product produced according to brands----
+SELECT brand_name, YEAR(order_date) year, CONVERT(MONEY,SUM(OI.list_price*quantity*(1-discount)),1) annual_amount
+FROM sale.order_item AS OI
+JOIN product.product AS P
+ON OI.product_id = P.product_id
+JOIN product.brand AS B
+ON P.brand_id = B.brand_id
+JOIN sale.orders AS O
+ON OI.order_id = O.order_id
+GROUP BY YEAR(order_date), brand_name
+ORDER BY annual_amount DESC
 
 
 ---- 11. Select the store which has the most sales quantity in 2016.----
+SELECT store_name, YEAR(O.order_date) sale_year, SUM(quantity) sale_quantity
+FROM sale.order_item AS OI
+JOIN sale.orders AS O
+ON OI.order_id = O.order_id
+JOIN sale.store AS S
+ON S.store_id = O.store_id
+GROUP BY S.store_name, YEAR(O.order_date)
+ORDER BY sale_quantity DESC
 
+SELECT YEAR(order_date), COUNT(*)
+FROM sale.order_item AS OI
+JOIN sale.orders AS O
+ON OI.order_id = O.order_id
+GROUP BY YEAR(order_date)
 
 ---- 12 Select the store which has the most sales amount in 2018.----
-
+SELECT TOP 1 store_name, YEAR(O.order_date) sale_year, SUM(list_price*quantity*(1-discount)) sale_amount
+FROM sale.order_item AS OI
+JOIN sale.orders AS O
+ON OI.order_id = O.order_id
+JOIN sale.store AS S
+ON S.store_id = O.store_id
+WHERE YEAR(O.order_date) = 2018
+GROUP BY S.store_name, YEAR(O.order_date)
+ORDER BY sale_amount DESC
 
 ---- 13. Select the personnel which has the most sales amount in 2019.----
-
+SELECT TOP 1 S.staff_id, S.first_name, S.last_name, YEAR(O.order_date) sale_year, SUM(list_price*quantity*(1-discount)) sale_amount
+FROM sale.order_item AS OI
+JOIN sale.orders AS O
+ON OI.order_id = O.order_id
+JOIN sale.staff AS S
+ON S.store_id = O.store_id
+WHERE YEAR(O.order_date) = 2019
+GROUP BY S.staff_id, S.first_name, S.last_name, YEAR(O.order_date)
+ORDER BY sale_amount DESC
