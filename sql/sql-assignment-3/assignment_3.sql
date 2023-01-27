4656f3e7e91d949514d617a375097b67e80d21ac
Selman <selmank@gmail.com>
2023-01-27 18:26:59 +0100

    added second solution with avg

--- sql/sql-assignment-3/assignment_3.sql
+++ sql/sql-assignment-3/assignment_3.sql
@@ -27,7 +27,7 @@ FROM
     sale.order_item OI ON P.product_id = OI.product_id
     JOIN
     sale.orders O ON OI.order_id = O.order_id
-ORDER BY product_id, order_date
+ORDER BY product_id, discount
 
 SELECT 
     P.product_id, SUM(quantity) total_quantity, discount
@@ -48,6 +48,58 @@ ORDER BY product_id, discount
         JOIN
         sale.order_item OI ON P.product_id = OI.product_id
     GROUP BY P.product_id, discount
+),
+summary AS (
+  SELECT 
+    product_id,
+    discount as x,
+    total_sale as y,
+    discount * total_sale as xy,
+    discount * discount as x_squared
+  FROM sales_data
+),
+summary_regression AS (
+  SELECT 
+    product_id,
+    SUM(y) as sum_y, 
+    SUM(x) as sum_x, 
+    SUM(xy) sum_xy, 
+    SUM(x_squared) as sum_x_squared, 
+    COUNT(x) as n
+  FROM summary
+  GROUP BY product_id
+)
+,regression AS (
+  SELECT 
+    product_id,
+    CASE
+        WHEN (n*sum_x_squared - sum_x * sum_x) <> 0 THEN (n*sum_xy - sum_x * sum_y) / (n*sum_x_squared - sum_x * sum_x)
+        ELSE NULL
+    END as slope
+  FROM summary_regression
+)
+--SELECT * FROM regression ORDER BY product_id
+SELECT 
+  product_id,
+  CASE 
+    WHEN slope > 0 THEN 'Positive'
+    WHEN slope < 0 THEN 'Negative'
+    ELSE 'Neutral'
+  END as effect
+FROM regression
+ORDER BY product_id
+
+-- Second Solution with average
+
+;WITH sales_data AS (
+    SELECT 
+        P.product_id, AVG(quantity * 1.0) total_sale, discount
+    FROM 
+        product.product P
+        JOIN
+        sale.order_item OI ON P.product_id = OI.product_id
+    GROUP BY P.product_id, discount
+    --ORDER BY product_id
 ),
 summary AS (
   SELECT 
