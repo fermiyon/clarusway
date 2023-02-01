c6d0f51e3a50fea5b6c3876e932693a876e8d4a6
Selman <selmank@gmail.com>
2023-02-01 18:09:17 +0100

    assignment 4 update

---
+++ sql/sql-assignment-4/assignment_4_drop.sql
@@ -0,0 +1,15 @@
+---DROPPING
+DECLARE @DatabaseName nvarchar(50)
+SET @DatabaseName = N'Manufacturer'
+
+DECLARE @SQL varchar(max)
+
+SELECT @SQL = COALESCE(@SQL,'') + 'Kill ' + Convert(varchar, SPId) + ';'
+FROM MASTER..SysProcesses
+WHERE DBId = DB_ID(@DatabaseName) AND SPId <> @@SPId
+
+--SELECT @SQL 
+EXEC(@SQL)
+
+-- DROP
+DROP DATABASE Manufacturer
